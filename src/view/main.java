0dbdaee0210c3ad9018a4449fb5693fa693b89a6
fvRaissa <raissafariasnet@hotmail.com>
2025-03-05 23:31:59 -0300

    ajustes

--- src/view/main.java
+++ src/view/main.java
@@ -1,5 +1,6 @@
 package view;
 
+import javax.swing.JOptionPane;
 import controller.RedesController;
 import java.util.Scanner;
 
@@ -13,13 +14,18 @@ public class main {
 		int opc;
 		
 		do {
-			System.out.println("Escolha a opção desejada: \n  1-ip\n  2-ping\n  9-encerrar");
-			opc = in.nextInt();
+			
+			opc = Integer.parseInt(JOptionPane.showInputDialog(" Escolha a opção desejada:  \n  1-ip \n  2-ping\n  9-encerrar"));
+			
 			switch (opc) {
 			case 1:
 				redesController.ip();
+				break;
 			case 2:
 				redesController.ping();
+				break;
+			case 9:
+				System.exit(0);
 			}
 		}while(opc != 9);
 		
